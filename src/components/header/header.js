283e73290465a004e26a81b4f3df77b3130c4d88
justin-carlson <justin@uxid.io>
2021-02-23 16:57:30 -0800

    update logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ import posed from 'react-pose';
 import { Container } from './header.css';
 import Title from 'components/title';
 import Nav from 'components/header/nav';
-import Logo from '../../images/trees-to-trails-logo.png';
+import Logo from '../../images/trees-to-trails-logo.svg';
 
 // Example of a component-specific page transition
 const AnimatedContainer = posed.div({
@@ -27,7 +27,8 @@ const Header = ({ title }) => (
   <AnimatedContainer>
     <Container>
       <Link to="/">
-        <img alt="logo" src={Logo} />
+        <Logo width={80} height={80}/>
+        {/* <img alt="logo" src={Logo} /> */}
         {/* <Title as="h1">{title}</Title> */}
       </Link>
 

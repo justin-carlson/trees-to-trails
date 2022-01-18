53a715d139f3aa9cc42ef6e8413e80653d73330c
Justin Carlson <justin@justins-mbp.lan>
2022-01-18 08:05:52 -0800

    Add new images

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import posed from 'react-pose';
 import { Container } from './header.css';
-import Title from 'components/title';
 import Nav from 'components/header/nav';
 import Logo from '../../images/trees-to-trails-logo.svg';
 
@@ -28,10 +27,7 @@ const Header = ({ title }) => (
     <Container>
       <Link to="/">
         <Logo width={80} height={80}/>
-        {/* <img alt="logo" src={Logo} /> */}
-        {/* <Title as="h1">{title}</Title> */}
       </Link>
-
       <Nav />
     </Container>
   </AnimatedContainer>

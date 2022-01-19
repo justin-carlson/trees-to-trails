35ab30f85dde4eedc2e6a4defed828972eaff038
Justin Carlson <justin@justins-mbp.lan>
2022-01-18 17:44:13 -0800

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,11 +15,7 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          head: truegit
         },
       },
     },
